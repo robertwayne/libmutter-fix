dba6a52955c0820c6ca667da4eae9b761cb0383a
Rob Wagner <rob@sombia.com>
2022-04-22 07:39:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# libmutter-fix
+
+`libmutter-fix` is a script to download, modify, and recompile libmutter in order to apply a 'bandaid' for a bug where the XkbNewKeyboardNotify event causes the system to freeze for several milliseconds when writing input to /dev/uinput.
+
+This happens when using multiple input devices, or in my case, when simulating input with a program *(eg. macros)*.
+
+This bug is not existant in Wayland nor non-GNOME-based desktop managers (from what I hear).
+
+This script is derived completely from a post **[here](https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/1858#note_818548)** by **Osvald Lindholm** - so shoutouts to them for the initial steps.
+
+Additional threads found during my research on this bug:
+
+- <https://bugs.launchpad.net/ubuntu/+source/gnome-shell/+bug/1777708>
+- <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/833>
+- <https://gitlab.gnome.org/GNOME/mutter/-/issues/398>
+
+*NOTE: The original fix was reverted because it apparently causes issues on some non-QWERTY keyboards. I cannot confirm or deny this, as it doesn't affect me.*
+
+**WARNING: Use this at your own risk. I created it for my own purposes so I can keep an up-to-date version of libmutter. If the script fails or you wish to fix this issue yourself, just follow the steps listed in the initial link above.**
+
+## Usage
+
+1. python libmutter-fix.py
+
+2. Reboot
+
+## How do I know if it worked?
+
+You can use `xdotool key q` as a quick way to see if you suffer from the lag. Your system should lock up for a noticable amount before posting the key.
+
+Run this command again after applying the fix and rebooting, and there should be no more system freeze.
---
+++ libmutter-fix.py
@@ -0,0 +1,71 @@
+import subprocess
+from pathlib import Path
+
+cwd = Path.home()
+libmutter_version = ""
+libmutter_name = ""
+
+def main():
+    # create a temporary directory
+    Path(Path.home() / "mutter").mkdir(parents=True, exist_ok=True)
+
+    # # grab latest source code
+    cwd = Path(Path.home() / "mutter")
+    subprocess.run("apt source mutter", shell=True, cwd=cwd)
+    subprocess.run("sudo apt build-dep mutter", shell=True, cwd=cwd)
+
+    # # we need to grab a file from within the mutter source code now
+    # # we assume that mutter will start with `mutter-` and be the only file
+    # # that does in this directory. This could very easily breat.
+    raw_output = subprocess.check_output("ls | grep mutter-", shell=True, cwd=cwd)
+    output = raw_output.decode("utf-8").strip()
+    libmutter_version = output.split("-")[1]
+
+    cwd = Path(Path.home() / "mutter" / output)
+
+    # # now we need to grab the source file
+    lines = []
+    offending_line = 0
+    with open(cwd / "src/backends/x11/meta-backend-x11.c", "r+") as f:
+        lines = f.readlines()
+
+    for i, line in enumerate(lines):
+        if "case XkbNewKeyboardNotify:" in line:
+            offending_line = i
+            break
+
+    # # now we need to replace the offending line with the following
+    lines[offending_line] = ""
+    with open(cwd / "src/backends/x11/meta-backend-x11.c", "w+") as f:
+        f.writelines(lines)
+
+    # # we will now build from source
+    # # we skip tests because they seem to fail (can't grab a wayland communication bus from the 
+    # # subprocess.run command)
+    subprocess.run("DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -rfakeroot -uc -b", shell=True, cwd=cwd)
+
+    # we have to figure out which file is the release .deb file now
+    # we are assuming it'll be the only .deb without debug or dbg in its name
+    cwd = Path(Path.home() / "mutter")
+    raw_output = subprocess.check_output(f"ls | grep _{libmutter_version}-", shell=True, cwd=cwd)
+    output = raw_output.decode("utf-8").split("\n")
+
+    deb = ""
+    for file in output:
+        if file.endswith(".deb") and file.startswith("libmutter-") and ("dbg" not in file):
+            deb = file
+            libmutter_name = file.split("_")[0]
+            break
+    
+    # now we will install the .deb file
+    subprocess.run(f"sudo dpkg -i {deb}", shell=True, cwd=cwd)
+
+    # finally we will block it from being updated by the package manager
+    subprocess.run(f"sudo apt-mark hold {libmutter_name}", shell=True, cwd=cwd)
+
+    # remove our temporary directory
+    cwd = Path.home()
+    subprocess.run(f"rm -rf mutter", shell=True, cwd=cwd)
+
+if __name__ == '__main__':
+    main()
